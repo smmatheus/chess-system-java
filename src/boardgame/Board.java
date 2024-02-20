4afb274d036f5059959b0819ab1fccb29a5f012b
Matheus Morais <matheusssm01@gmail.com>
2024-02-20 13:51:53 -0300

    Placing and pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,4 +35,9 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()] [position.getCollum()];
 	}
+	
+	public void placePiece(Piece piece,Position position) {
+		pieces[position.getRow()][position.getCollum()] = piece;
+		piece.position = position;
+	}
 }
