f97299cd413904db87066f9d9e9edc64eb6a4ba0
Matheus Morais <matheusssm01@gmail.com>
2024-02-27 11:46:56 -0300

    Movimentos possíveis de uma peça

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -13,7 +13,23 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-
 	
+	public abstract boolean [][] possibleMove();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMove()[position.getRow()] [position.getCollum()];
+	}
+ 	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMove();
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 }
